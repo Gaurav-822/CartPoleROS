033a2473a243af44ca82878e2ce3528c5e3b1973
Gaurav-822 <gaurav.moocs@gmail.com>
2026-02-01 14:52:43 +0000

    enabling wrench (to apply commands to robot from ros) via world enabling that (can't be done by urdf)

--- src/cartpole_sim/launch/sim.launch.py
+++ src/cartpole_sim/launch/sim.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, TimerAction
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -10,22 +10,37 @@ def generate_launch_description():
     pkg_path = get_package_share_directory('cartpole_sim')
     urdf_path = os.path.join(pkg_path, 'urdf', 'cartpole.urdf')
 
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': open(urdf_path).read()
+        }]
+    )
+
+    gazebo = ExecuteProcess(
+        cmd=['gz', 'sim', '-r', 'empty.sdf'],
+        output='screen'
+    )
+
+    spawn = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'cartpole',
+            '-topic', 'robot_description'
+        ],
+        output='screen'
+    )
+
+    delayed_spawn = TimerAction(
+        period=2.0,
+        actions=[spawn]
+    )
+
     return LaunchDescription([
-
-        # Start Gazebo Harmonic
-        ExecuteProcess(
-            cmd=['gz', 'sim', '-r', 'empty.sdf'],
-            output='screen'
-        ),
-
-        # Spawn cartpole
-        Node(
-            package='ros_gz_sim',
-            executable='create',
-            arguments=[
-                '-name', 'cartpole',
-                '-file', urdf_path
-            ],
-            output='screen'
-        ),
+        robot_state_publisher,
+        gazebo,
+        delayed_spawn
     ])
